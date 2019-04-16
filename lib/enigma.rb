4174f342b3731ec96767197c8e6e807c7505bccd
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-15 18:55:04 -0600

    added shell of hash expected for encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,11 @@
 class Enigma
 
   def encrypt(message, key, offset)
-    
+    {
+      encryption: message,
+      key: key,
+      offset: offset,
+    }
   end
 
   def decrypt(message, key, offset)
