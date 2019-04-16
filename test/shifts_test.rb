384f813dc67be2f932eb49a48e6d311291cc2613
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 14:17:17 -0600

    added tests "test_it_initiatse_with_given_attributes" and "test_it_can_initiate_without_given_attributes" to shifts_test

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -3,11 +3,24 @@ require_relative 'test_helper'
 class ShiftsTest < MiniTest::Test
 
   def setup
-    @shift = Shifts.new("02715", "040895")
+    @shift_1 = Shifts.new("02715", "040895")
+    @shift_2 = Shifts.new
   end
 
   def test_it_exists
-    assert_instance_of Shift, @shift
+    assert_instance_of Shifts, @shift_1
+    assert_instance_of Shifts, @shift_2
+  end
+
+  def test_it_initiatse_with_given_attributes
+    assert_equal "02715", @shift_1.key
+    assert_equal "040895", @shift_1.offset
+    assert_equal 27, @shift_1.characters.count
+  end
+
+  def test_it_can_initiate_without_given_attributes
+    assert_instance_of Key, @shifts_2.key
+    assert_instance_of Offset, @shifts_2.offset
   end
 
 end
