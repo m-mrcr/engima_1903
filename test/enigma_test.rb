9eb1e406fdc9efb16aa51412896732a6c9462ab9
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-15 18:15:52 -0600

    added tests for encrypt/decrypt methods with provided message.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,4 +10,22 @@ class EnigmaTest < MiniTest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_encrypt_a_provided_message
+    expected = { encryption: "keder ohulw",
+                 key: "02715",
+                 date: "040895"
+               }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_a_provided_encrypted_message
+    expected = { decryption: "hello world",
+                 key: "02715",
+                 date: "040895"
+                }
+
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
 end
