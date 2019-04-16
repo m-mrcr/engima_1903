d13bc78832fa79ac0d336ede4bd3bf6a72b251b7
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 15:52:55 -0600

    added split method for offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -7,6 +7,14 @@ class Offset
     @split = split
   end
 
-
+  def split
+   final = {}
+   setting = (@initial.to_i ** 2).to_s.chars.last(4)
+   final[:a] = setting[0].to_i
+   final[:b] = setting[1].to_i
+   final[:c] = setting[2].to_i
+   final[:d] = setting[3].to_i
+   final
+  end
 
 end
