ff26d9f8ba1fbbdd174850e6d8b35eb27d72ae6d
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 15:26:55 -0600

    added implementation of split method and small changes to other func.

--- lib/key.rb
+++ lib/key.rb
@@ -1,8 +1,20 @@
 class Key
 
-  def initialize(key = rand.to_s[2..6])
-    @key = key
+  attr_reader :initial,
+              :split
+
+  def initialize(input = "%05d"%rand(99999))
+    @initial = input
     @split = split
+  end
+
+  def split
+    final = {}
+    final[:a] = @initial[0..1].to_i
+    final[:b] = @initial[1..2].to_i
+    final[:c] = @initial[2..3].to_i
+    final[:d] = @initial[3..4].to_i
+    final
   end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -8,34 +8,25 @@ class KeyTest < MiniTest::Test
   end
 
   def test_it_exists
-    assert_instance_of Key, @key
+    assert_instance_of Key, @key_1
   end
 
   def test_it_initiates_with_given_variables
-    assert_equal "02715", @key_1
+    assert_equal "02715", @key_1.initial
   end
 
   def test_it_initiates_without_given_variables
-    assert_instance_of String, @key_2
-    assert_isntance_of Integer, @key_2.to_i
-    assert_equal 5, @key_2.chars.count
+    assert_instance_of String, @key_2.initial
+    assert_instance_of Integer, @key_2.initial.to_i
+    assert_equal 5, @key_2.initial.chars.count
   end
 
   def test_it_can_split_into_useful_hash
     expected = {
-                a: 2,
+                a: 02,
                 b: 27,
                 c: 71,
                 d: 15
-               }
-
-    assert_equal expected, @key_1.split
-
-    expected = {
-                a: @key_2[0,1],
-                b: @key_2[1,2],
-                c: @key_2[2,3],
-                d: @key_2[3,4]
                }
 
     assert_equal expected, @key_1.split
