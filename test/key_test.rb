93160adef7998b12ad1b73162afd9cf3d1553f85
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 14:31:44 -0600

    added tests for intended functionality in initialize method in key class

--- test/key_test.rb
+++ test/key_test.rb
@@ -3,11 +3,22 @@ require_relative 'test_helper'
 class KeyTest < MiniTest::Test
 
   def setup
-    @key = Key.new
+    @key_1 = Key.new("02715")
+    @key_2 = Key.new
   end
 
   def test_it_exists
     assert_instance_of Key, @key
+  end
+
+  def test_it_initiates_with_given_variables
+    assert_equal "02715", @key_1
+  end
+
+  def test_it_initiates_without_given_variables
+    assert_instance_of String, @key_2
+    assert_isntance_of Integer, @key_2.to_i
+    assert_equal 5, @key_2.chars.count
   end
 
 end
