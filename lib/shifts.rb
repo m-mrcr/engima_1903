227e66e05b379d7368ffa9f6174fa9ae84167d75
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 16:11:55 -0600

    added initialize method to shifts class

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,3 +1,10 @@
 class Shifts
 
+  def initialize(key = Key.new, offset = Offset.new)
+    @key = key.class == String ? Key.new(key) : key
+    @offset = date.class == String ? Offset.new(date) : date
+  end
+
+  
+
 end
