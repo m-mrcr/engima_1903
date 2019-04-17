afc0427f709fa8382a976908d40930262cd6ce43
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 21:18:58 -0600

    creates the message meant to print to the terminal

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -11,3 +11,5 @@ encrypted = File.open(write_file, "w")
 encrypted_message = Enigma.new.encrypt(message.read.chomp)
 encrypted.write(encrypted_message[:encryption])
 encrypted.close
+
+p "Created #{write_file} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
