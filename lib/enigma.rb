a33a347451ac4b04a77ef2a46c0893b3613c4489
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 18:51:16 -0600

    added more to the shells of encrypt/decrypt (mirrored implementation)

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,18 +1,20 @@
 class Enigma
 
-  def encrypt(message, key, offset)
+  def encrypt(message, key = Key.new, offset = Offset.new)
+    shifts = Shifts.new(key, offset)
     {
       encryption: message,
-      key: key,
-      date: offset,
+      key: key.initial_key,
+      date: offset.initial_offset,
     }
   end
 
-  def decrypt(message, key, offset)
+  def decrypt(message, key = Key.new, offset = Offset.new)
+    shifts = Shifts.new(key, offset)
     {
       decryption: message,
-      key: key,
-      date: offset,
+      key: key.initial_key,
+      date: offset.initial_offset,
     }
   end
 
