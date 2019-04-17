24c55c75c65e2a00ba913427b151dfc7db0bc10d
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 21:38:25 -0600

    created functionality that should decrypt the message, now debugging

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -11,3 +11,5 @@ decryption_location = File.open(decrypt, "w")
 encrypted_message = Enigma.new.decrypt(encrypted_message.read, key, offset)
 created.write(encrypted_message[:decryption])
 created.close
+
+p "Created #{decrypt} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
