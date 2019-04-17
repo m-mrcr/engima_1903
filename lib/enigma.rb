7289ace3ccc631812dcad367bb940c3940011ca6
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-16 21:57:34 -0600

    found some stray commas, maybe that's why things aren't working.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,20 +2,16 @@ class Enigma
 
   def encrypt(message, key = Key.new, offset = Offset.new)
     shifts = Shifts.new(key, offset)
-    {
-      encryption: shifts.encode(message),
+    { encryption: shifts.encode(message),
       key: shifts.key.initial_key,
-      date: shifts.offset.initial_offset,
-    }
+      date: shifts.offset.initial_offset }
   end
 
   def decrypt(message, key = Key.new, offset = Offset.new)
     shifts = Shifts.new(key, offset)
-    {
-      decryption: shifts.decode(message),
+    { decryption: shifts.decode(message),
       key: shifts.key.initial_key,
-      date: shifts.offset.initial_offset,
-    }
+      date: shifts.offset.initial_offset }
   end
 
 end
